f1420962d3dc0e70a42a722e92d3f3c344dfb63b
Júlia Rosa <julia.chdck@gmail.com>
2025-09-17 21:37:32 -0300

    verificar se número é positivo, negativo ou zero
    Adiciona script que lê um número do usuário e informa
    se ele é positivo, negativo ou zero.

---
+++ Python/revisao2_06.py
@@ -0,0 +1,8 @@
+# ler um numero e dizer se ele é positivo, negativo ou zero
+num = float(input("Digite um número: "))
+if num > 0:
+    print("Positivo")
+elif num < 0:
+    print("Negativo")
+else:
+    print("Zero")
