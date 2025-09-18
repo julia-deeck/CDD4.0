0edb580d689796d38ff417a30f46002a7ff9867f
Júlia Rosa <julia.chdck@gmail.com>
2025-09-17 21:29:06 -0300

    testar funções utilitárias da biblioteca
    Adiciona programa de testes para funções implementadas em
    biblioteca_aula07, incluindo:
    
    - Contagem de vogais em frase
    - Cálculo de valor em estoque
    - Verificação de número inteiro (P/N/Z)
    - Soma de dois ou múltiplos números
    - Impressão de texto ao contrário com contagem de letras
    - Remoção de repetições em lista

---
+++ Python/main_aula07.py
@@ -0,0 +1,16 @@
+from biblioteca_aula07 import *
+
+vogaisQuant("O rato roeu a roupa do rei de roma")
+resposta = valorEstoque("Chocolate", 2,5 )
+print(resposta[0], resposta[1])
+
+resposta2 = numInteiro(0)
+print(resposta2)
+
+somaNumeros(1,2)
+
+somar(1,2,3)
+
+letrasSomaContrario("Olá Mundo")
+
+listaRepeticao([10,12,12,31,4,4,5,31,6,7,6,8])
