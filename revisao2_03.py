21a33e1148a30ceb53d2a4a3da147868adf7f224
Júlia Rosa <julia.chdck@gmail.com>
2025-09-17 21:39:11 -0300

    imprimir números pares de 1 a 20 e 20 a 1
    Adiciona script que imprime todos os números pares:
    - Contagem decrescente de 20 a 1
    - Contagem crescente de 1 a 20

---
+++ revisao2_03.py
@@ -0,0 +1,9 @@
+# imprimir todos os numeros pares de 20 a 1
+for i in range(21, 1, -1):
+    if i % 2 == 0:
+        print(i)
+
+# imprimir todos os numeros pares de 1 a 20
+for i in range(1,21):
+    if i % 2 == 0:
+        print(i)
