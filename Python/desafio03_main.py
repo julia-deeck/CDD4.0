8e661b7ca23c1a1ee30fa2037ae22da526aa0a50
Júlia Rosa <julia.chdck@gmail.com>
2025-09-17 21:28:08 -0300

    testes com classes de atletas
    Adiciona programa que instancia e utiliza classes de atletas:
    - Corredor
    - Nadador
    - Ciclista
    - Triatleta
    
    Cada atleta executa métodos de aquecimento, aposentadoria e
    ações específicas de sua modalidade.

---
+++ Python/desafio03_main.py
@@ -0,0 +1,23 @@
+from desafio03_biblioteca import *
+
+c = Corredor(55)
+c.atletaAposentado()
+c.aquecerAtleta()
+c.correr()
+
+n = Nadador(70)
+n.atletaAposentado()
+n.aquecerAtleta()
+n.nadar()
+
+ci = Ciclista(80)
+ci.atletaAposentado()
+ci.aquecerAtleta()
+ci.pedalar()
+
+t = Triatleta(75)
+t.atletaAposentado()
+t.aquecerAtleta()
+t.correr()
+t.nadar()
+t.pedalar()
