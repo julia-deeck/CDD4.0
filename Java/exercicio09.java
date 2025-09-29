bdddf882cfe55070cfffd94c486aed12a6d55d18
Júlia Rosa <julia.chdck@gmail.com>
2025-09-29 20:02:12 -0300

    imprime todos os valores de um array de doubles
    Adiciona um exercício que percorre um array de números decimais
    e imprime cada valor na tela usando um loop for-each.

---
+++ Java/exercicio09.java
@@ -0,0 +1,12 @@
+package aula03;
+
+public class exercicio09 {
+
+	public static void main(String[] args) {
+		double lista[] = {10, 2, 4, 5, 5.6, 2.1}; // cria um array com valores decimais
+		
+		for(double x : lista) { // percorre cada elemento do array
+			System.out.println(x + " "); // exibe o valor do elemento
+		}
+	}
+}
