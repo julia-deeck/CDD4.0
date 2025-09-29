07b37a7ae5b6ce052065f104a2ada742a72cf11e
Júlia Rosa <julia.chdck@gmail.com>
2025-09-29 19:57:24 -0300

    imprimir números ímpares de 0 a 50
    Implementa laço para imprimir todos os números ímpares entre 0 e 50 usando for.

---
+++ Java/exercicio02.java
@@ -0,0 +1,12 @@
+package aula03;
+
+public class exercicio02 {
+
+	public static void main(String[] args) {
+		for (int i = 0; i <= 50; i++) { // percorre números de 0 a 50
+			if (i % 2 == 1) { // verifica se o número é ímpar
+				System.out.println(i); // exibe o número ímpar
+			}
+		}
+	}
+}
