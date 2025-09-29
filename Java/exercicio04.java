3d7f766b4c1f2b7f1463719a63332727575faf11
Júlia Rosa <julia.chdck@gmail.com>
2025-09-29 19:58:44 -0300

    imprimir múltiplos de 3 de 1 a 100
    Implementa um laço for que percorre de 1 a 100 e imprime apenas os números múltiplos de 3.

---
+++ Java/exercicio04.java
@@ -0,0 +1,12 @@
+package aula03;
+
+public class exercicio04 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 100; i++) { // percorre números de 1 até 100
+			if (i % 3 == 0) { // verifica se o número é múltiplo de 3
+				System.out.println(i); // exibe o número múltiplo de 3
+			}
+		}
+	}
+}
