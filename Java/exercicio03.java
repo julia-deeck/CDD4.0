b812b9238ea5a7b965539fb273d2921da7cfb4b4
Júlia Rosa <julia.chdck@gmail.com>
2025-09-29 19:58:04 -0300

    imprimir números de 10 a 1 em ordem decrescente
    Implementa um laço for que imprime os números de 10 até 1 em ordem decrescente.

---
+++ Java/exercicio03.java
@@ -0,0 +1,10 @@
+package aula03;
+
+public class exercicio03 {
+
+	public static void main(String[] args) {
+		for (int i = 10; i >= 1; i--) { // percorre números de 10 até 1, decrementando a cada iteração
+			System.out.println(i); // exibe o número atual
+		}
+	}
+}
